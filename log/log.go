5c75761c2cbc66af1f908bd4eeab8922dd627626
Krzysztof Wiatrzyk <krzysztof.wiatrzyk@airhelp.com>
2023-01-23 12:44:56 +0100

    feat(flag): add fail-if-missing flag and verbose mode

---
+++ log/log.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func InitLogger(logLevel string) {
+
+	var zapLogLevel zapcore.Level
+
+	switch logLevel {
+	case "debug":
+		zapLogLevel = zap.DebugLevel
+	default: 
+		zapLogLevel = zap.InfoLevel
+	}
+
+	zapConfig := zap.NewDevelopmentConfig()
+	zapConfig.Level.SetLevel(zapLogLevel)
+	logger, _ := zapConfig.Build()
+	zap.ReplaceGlobals(logger)
+}
